77ff5d50740473a655ea1116825534efcc04c3b9
michael ontiveros <michael.david.ontiveros@gmail.com>
2024-08-22 13:34:07 -0600

    Describe lattice key encapsulation.

--- poppy/__init__.py
+++ poppy/__init__.py
@@ -3,6 +3,7 @@ jax.config.update("jax_enable_x64", True)
 from poppy.constant import DTYPE, POLYNOMIAL, BLOCKSIZE, SEED
 from poppy.field import field
 from poppy.array import array, zeros, ones, arange, eye, random
+from poppy.modular import addmod, submod, mulmod
 from poppy.linear import trace, outer33, outer44, matmul55, mtrsm, trsm, mgetrf, getrf, gje2, inv, kerim
 from poppy.rep import transpose, block, unblock, int2vec, vec2int, int2mat, mat2int, vec2mat, mat2vec, rep
 from poppy.ring import Z2, M2
