10c2d5c951732b7bb1e0cb58f218e2da3d4bf047
michael ontiveros <michael.david.ontiveros@gmail.com>
2024-08-27 13:30:33 -0600

    Increase image resolution.

--- poppy/plot.py
+++ poppy/plot.py
@@ -2,7 +2,7 @@ import jax
 import matplotlib.pyplot
 
 def plot(a, title = '', size = 4, cmap = 'twilight_shifted'):
-    matplotlib.rc('figure', figsize=(size,size))
+    matplotlib.rc('figure', figsize = (size,size), dpi = 300)
     a = a.squeeze()
     s = jax.numpy.array(a.shape)
     matplotlib.pyplot.matshow(a.reshape((s[:len(s)//2].prod(),-1)), cmap = cmap, interpolation = 'none')
